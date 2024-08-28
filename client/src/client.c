6a773e95a39fd6aabfe93823285e06a220621d21
Luca <llorenzon@frba.utn.edu.ar>
2024-08-28 19:07:27 +0000

    A punto de la tercera etapa

--- client/src/client.c
+++ client/src/client.c
@@ -32,15 +32,15 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
-	ip = config_get_string_value(config,"ip");
-	puerto = config_get_string_value(config,"puerto");
-	valor = config_get_string_value(config,"valor");
+	ip = config_get_string_value(config,"IP");
+	puerto = config_get_string_value(config,"PUERTO");
+	valor = config_get_string_value(config,"CLAVE");
 
 	// Loggeamos el valor de config
 
-	log_info(logger,ip);
-	log_info(logger,puerto);
-	log_info(logger,valor);
+	log_info(logger,"%s",ip);
+	log_info(logger,"%s",puerto);
+	log_info(logger,"%s",valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -78,7 +78,7 @@ t_log* iniciar_logger(void)
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config = config_create("/home/utnso/Desktop/tp0/cliente.config");
+	t_config* nuevo_config = config_create("/home/utnso/Desktop/tp0/client/cliente.config");
 	if (nuevo_config  == NULL)
 	{
         abort();
@@ -88,13 +88,31 @@ t_config* iniciar_config(void)
 
 void leer_consola(t_log* logger)
 {
+
 	char* leido;
 
-	// La primera te la dejo de yapa
-	leido = readline("> ");
 
+	// La primera te la dejo de yapa
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-	log_info(logger,leido);
+
+
+	while(1)
+	{
+
+		leido = readline("> ");
+
+		
+		if (leido == NULL)
+		{
+			log_info(logger,"entre");
+			break;
+			
+		}
+
+		log_info(logger,"%s",leido);
+
+	}
+	
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
 	free(leido);
